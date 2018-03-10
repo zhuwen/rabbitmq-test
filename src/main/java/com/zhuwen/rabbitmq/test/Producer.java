0348c5e07813a9b339780966d1606c65f37a2dc5
zhuwen <zhuwen-new@163.com>
2018-03-11 00:23:11 +0800

    frist commit

---
+++ src/main/java/com/zhuwen/rabbitmq/test/Producer.java
@@ -0,0 +1,38 @@
+package com.zhuwen.rabbitmq.test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * 消息生成者
+ */
+public class Producer {
+	public final static String QUEUE_NAME = "rabbitMQ.test";
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		System.out.println("start...");
+		// 创建连接工厂
+		ConnectionFactory factory = new ConnectionFactory();
+		// 设置RabbitMQ相关信息
+		factory.setHost("10.42.251.37");
+		factory.setUsername("admin");
+		factory.setPassword("admin");
+		factory.setPort(5672);
+		// 创建一个新的连接
+		Connection connection = factory.newConnection();
+		// 创建一个通道
+		Channel channel = connection.createChannel();
+		// 声明一个队列 channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		String message = "Hello RabbitMQ";
+		// 发送消息到队列中
+		channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+		System.out.println("Producer Send +'" + message + "'");
+		// 关闭通道和连接
+		channel.close();
+		connection.close();
+	}
+}
